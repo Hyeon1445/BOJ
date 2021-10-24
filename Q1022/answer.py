6d5d125f82b2b0d826983b9216df8ad5bd68b700
Hyeon1445 <lily1445@gmail.com>
2021-10-24 13:41:27 +0900

    Q1022: 소용돌이 예쁘게 출력하기(python 정답)

--- Q1022/answer.py
+++ Q1022/answer.py
@@ -1,48 +1,17 @@
-r1, c1, r2, c2 = map(int, input().split(' '))
+from sys import stdin
+r1, c1, r2, c2 = map(int, stdin.readline().split(' '))
 
-m = max(-r1, r2, -c1, c2)
+def val(x, y):
+  if x == 0 and y == 0: return 1
+  lap = max(abs(x), abs(y))
+  if y == lap: return (2 * lap + 1)**2 - (lap - x)
+  elif x == -lap: return (2 * lap + 1)**2 - (2 * lap) - (lap - y)
+  elif y == -lap: return (2 * lap + 1)**2 - 2 * (2 * lap) - (lap + x)
+  else: return (2 * lap + 1)**2 - 3 * (2 * lap) - (lap + y)
 
-arr = [[0 for i in range(r2 - r1 + 1)] for j in range(c2 - c1 + 1)]
-x = m
-y = m
-count = 1
-if r1 + m < y and r2 + m > y and c1 + m < x and c2 + m > x:
-  arr[x][y] = count
+maxLen = max(len(str(val(c1, r1))), len(str(val(c2, r2))))
 
-for i in range(m):
-  count = count +1
-  x = x + 1
-  if r1 + m <= y and r2 + m >= y and c1 + m <= x and c2 + m >= x:
-    arr[x][y] = count
-  for j in range((i+1)*2-1):
-    count = count +1
-    y = y - 1
-    if r1 + m <= y and r2 + m >= y and c1 + m <= x and c2 + m >= x:
-      arr[x][y] = count
-  for j in range((i+1)*2):
-    count = count + 1
-    x = x - 1
-    if r1 + m <= y and r2 + m >= y and c1 + m <= x and c2 + m >= x:
-      arr[x][y] = count
-  for j in range((i+1)*2):
-    count = count + 1
-    y = y + 1
-    if r1 + m <= y and r2 + m >= y and c1 + m <= x and c2 + m >= x:
-      arr[x][y] = count
-  for j in range((i+1) * 2):
-    count = count + 1
-    x = x + 1
-    if r1 + m <= y and r2 + m >= y and c1 + m <= x and c2 + m >= x:
-      arr[x][y] = count
-
-      
-max_len = max(len(str(arr[c1 + m][r1 + m])), len(str(arr[c2 + m][r1 + m])))
-
-
-for i in range(r1 + m, r2 + m + 1):
-  for j in range(c1 + m, c2 + m + 1):
-    k = '                        ' + str(arr[j][i])
-    print(k[-max_len:], end = ' ')
+for i in range(r1, r2 + 1):
+  for j in range(c1, c2 + 1):
+    print(('%' + str(maxLen) + 'd') % val(j, i), end=' ')
   print()
-
-#런타임 에러
