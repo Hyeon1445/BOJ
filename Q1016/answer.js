6a3e35f435c449d1f7fdeedbb432b9d913dd6056
Hyeon1445 <lily1445@gmail.com>
2021-10-31 08:55:39 +0900

    Q1016: 제곱ㄴㄴ수(미완료)

---
+++ Q1016/answer.js
@@ -0,0 +1,6 @@
+const inp = require('fs').readFileSync('Q13241/input.txt').toString().trim().split(' ')
+// const inp = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ')
+
+const minNum = parseInt(inp[0])
+const maxNum = parseInt(inp[1])
+
