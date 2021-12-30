1b4f3f8d815337faf6982ceb039c454c7875b26f
Hyeon1445 <lily1445@gmail.com>
2021-12-31 00:39:50 +0900

    Q5430. AC (node.js answer)

--- Q5430/answer.js
+++ Q5430/answer.js
@@ -1,29 +1,30 @@
 function solveAC(arr, AC) {
   let isReversed = false
-  const ACLength = AC.length
-  for(let j = 0; j < ACLength; j++) {
-    if(AC.shift() === 'R') {
+  for (const j of AC) {
+    if (j === 'R') {
       isReversed = !isReversed
-    } else if (arr.length < 1) {
-      console.log('error')
-      return
-    } else {
-      isReversed ? arr.pop() : arr.shift()
+    } else if (j === 'D') {
+      if (arr.length < 1) {
+        console.log('error')
+        return
+      } else {
+        isReversed ? arr.pop() : arr.shift()
+      }
     }
   }
   const result = isReversed ? arr.reverse() : arr
-  console.log('[' + String(arr).trim() + ']')
+  console.log(JSON.stringify(result))
 }
 
-const inp = require('fs').readFileSync('Q5430/input.txt').toString().trim().split('\r\n')
-// const inp = require('fs').readFileSync('/dev/stdin').toString().trim().split('\r\n')
+// const inp = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+const inp = require('fs').readFileSync('Q5430/input.txt').toString().trim().split('\n')
+
 const n = +inp[0]
-for(let i = 0; i < n; i++) {
-  let AC = inp[i * 3 + 1].split('')
-  let num = +inp[i * 3 + 2]
-  let arr = inp[i * 3 + 3].slice(1, -1).length ? inp[i * 3 + 3].slice(1, -1).split(',') : []
-  arr = arr.map(a => +a)
-  solveAC(arr, AC) 
+for (let i = 1; i < inp.length; i += 3) {
+  let AC = inp[i].split("")
+  let arr = JSON.parse(inp[i + 2])
+  solveAC(arr, AC)
 }
 
-// wrong answer
+
+// 오답 원인: 입력 시 '/r/n'
