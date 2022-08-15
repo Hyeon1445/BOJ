2f722677fceb1702cddd57eb9cdfc8d8d2178f78
Hyeon1445 <lily1445@gmail.com>
2022-08-15 21:28:28 +0900

    Q244444. 알고리즘 수업 - 너비 우선 탐색1

---
+++ Q24444/answer.js
@@ -0,0 +1,36 @@
+const [[N, M, R], ...inputs] = require("fs").readFileSync("Q24479/input.txt").toString().trim().split("\r\n").map((input) => input.split(" ").map(Number))
+const visited = Array(N).fill(0)
+let index = 1
+const queue = []
+
+const graph = new Map()
+for(let i = 1; i <= N; i++) {
+  graph.set(i, [])
+}
+for(let i = 1; i <= M; i++) {
+  let [start, end] = inputs[i - 1]
+  graph.get(start).push(end)
+  graph.get(end).push(start)
+}
+graph.forEach((line) => {
+  line.sort((a, b) => a - b)
+})
+
+const bfs = (dot) => {
+  visited[dot - 1] = index
+  index++
+  queue.unshift(dot)
+  while(queue.length) {
+    const u = queue.pop()
+    graph.get(u).forEach(v => {
+      if(visited[v - 1] === 0) {
+        visited[v - 1] = index
+        index++
+        queue.unshift(v)
+      }
+    })
+  }
+}
+
+bfs(R)
+console.log(visited.join('\n'))
