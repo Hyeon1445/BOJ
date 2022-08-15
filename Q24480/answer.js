e9d307950a55a8962260ca94f79c2e9087b09e29
Hyeon1445 <lily1445@gmail.com>
2022-08-15 19:51:41 +0900

    Q24480. 알고리즘 수업 - 깊이 우선 탐색2

---
+++ Q24480/answer.js
@@ -0,0 +1,28 @@
+const [[N, M, R], ...inputs] = require("fs").readFileSync("Q24479/input.txt").toString().trim().split("\r\n").map((input) => input.split(" ").map(Number))
+const visited = Array(N).fill(0)
+let index = 1
+const graph = new Map()
+for(let i = 1; i <= N; i++) {
+  graph.set(i, [])
+}
+for(let i = 1; i <= M; i++) {
+  let [start, end] = inputs[i - 1]
+  graph.get(start).push(end)
+  graph.get(end).push(start)
+}
+graph.forEach((line) => {
+  line.sort((a, b) => b - a)
+})
+
+const dfs = (dot) => {
+  visited[dot - 1] = index
+  index++
+  graph.get(dot).forEach((edge) => {
+    if (!visited[edge - 1]) {
+      dfs(edge)
+    }
+  })
+}
+
+dfs(R)
+console.log(visited.join("\n"))
